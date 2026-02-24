b6b89c5a4aba84b6933c0f7ef18e20b324c44432
alecamsan1 <alecamsan@alum.us.es>
2026-02-24 13:19:19 +0100

    .

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path"); 
 const app = express();
 const port = process.env.PORT || 10000;
+const cool = require("cool-ascii-faces");
 
 app.use(express.json());
 
@@ -12,7 +13,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "about.html"));
 });
-// 1. Datos iniciales
+// 1. Datos iniciales ACS
 let drinking_water_services = [
     { entity: "Afghanistan", code: "AFG", year: 2000, wat_bas_pop_residence_urban: 1564933.9 },
     { entity: "Afghanistan", code: "AFG", year: 2001, wat_bas_pop_residence_urban: 1583404 },
@@ -39,13 +40,17 @@ app.get("/samples/ACS", (req, res) => {
     res.send(`La media de wat_bas_pop_residence_urban en Algeria es ${media}`);
 });
 
-
+//Aqui empiezan los get
 
 app.get("/api/v1/drinking-water-services", (req, res) => {
     let filtrados = [...drinking_water_services];
+    //otra forma posible 
+    // let filtrados= drinking_water_services.slice();
     const { entity, year, from, to } = req.query;
+    //Es lo mismo que hacer const entity=req.query.entity; etc
 
-    if (entity) filtrados = filtrados.filter(d => d.entity.toLowerCase() === entity.toLowerCase());
+
+    if (entity) filtrados = filtrados.filter(d => d.entity.toLocaleLowerCase()===entity.toLocaleLowerCase());
     if (year) filtrados = filtrados.filter(d => d.year == year);
     if (from && to) filtrados = filtrados.filter(d => d.year >= from && d.year <= to);
 
@@ -53,7 +58,8 @@ app.get("/api/v1/drinking-water-services", (req, res) => {
 });
 
 app.get("/api/v1/drinking-water-services/loadInitialData", (req, res) => {
-    if (drinking_water_services.length === 0) {
+    if (drinking_water_services.length === 0) //Es decir no hay ningun elemento
+    {
         drinking_water_services = [
             { entity: "Afghanistan", code: "AFG", year: 2000, wat_bas_pop_residence_urban: 1564933.9 },
             { entity: "Afghanistan", code: "AFG", year: 2001, wat_bas_pop_residence_urban: 1583404 },
@@ -74,13 +80,17 @@ app.get("/api/v1/drinking-water-services/loadInitialData", (req, res) => {
 
 app.post("/api/v1/drinking-water-services", (req, res) => {
     const newData = req.body;
+    //Comprueba si falta un campo
     if (!newData || !newData.entity || !newData.year || !newData.code) {
         res.status(400).send("Bad Request: Faltan campos.");
     } else {
+        //Si no falta un campo 
         const existe = drinking_water_services.some(d => d.entity === newData.entity && d.year === newData.year);
         if (existe) {
-            res.status(409).send("Conflict: El recurso ya existe.");
+            //Si ya existe evita duplicados
+            res.status(409).send("Conflicto: El recurso ya existe.");
         } else {
+            //Si no existe lo añade
             drinking_water_services.push(newData);
             res.sendStatus(201); 
         }
@@ -88,7 +98,7 @@ app.post("/api/v1/drinking-water-services", (req, res) => {
 });
 
 app.delete("/api/v1/drinking-water-services", (req, res) => {
-    drinking_water_services = [];
+    drinking_water_services = [];//Array vacio 
     res.sendStatus(200); 
 });
 
@@ -97,7 +107,7 @@ app.get("/api/v1/drinking-water-services/:entity/:year", (req, res) => {
     const { entity, year } = req.params;
     const recurso = drinking_water_services.find(d => d.entity.toLowerCase() === entity.toLowerCase() && d.year == year);
     if (recurso) {
-        res.status(200).json(recurso); // Devuelve OBJETO {}
+        res.status(200).json(recurso); // Devuelve objeto {}
     } else {
         res.status(404).send("Not Found");
     }
@@ -108,10 +118,12 @@ app.put("/api/v1/drinking-water-services/:entity/:year", (req, res) => {
     const updatedData = req.body;
     
     if (updatedData.entity !== entity || updatedData.year != year) {
+        //Si los datos no coinciden con los actuales
         return res.status(400).send("Bad Request: Los datos no coinciden.");
     }
 
     const index = drinking_water_services.findIndex(d => d.entity.toLowerCase() === entity.toLowerCase() && d.year == year);
+   //Si existe el elemento
     if (index !== -1) {
         drinking_water_services[index] = updatedData;
         res.sendStatus(200);
@@ -125,6 +137,7 @@ app.delete("/api/v1/drinking-water-services/:entity/:year", (req, res) => {
     const initialLength = drinking_water_services.length;
     drinking_water_services = drinking_water_services.filter(d => !(d.entity.toLowerCase() === entity.toLowerCase() && d.year == year));
     
+    //Si el tamaño es distinto que el inicial significa que filtró
     if (drinking_water_services.length < initialLength) {
         res.sendStatus(200);
     } else {
@@ -138,4 +151,9 @@ app.put("/api/v1/drinking-water-services", (req, res) => res.sendStatus(405));
 
 app.listen(port, () => {
     console.log(`Servidor funcionando en puerto ${port}`);
+});
+
+app.get("/cool", (req, res) => {
+    console.log("Nueva petición a /cool");
+    res.send(`<html><body><h1>" ${cool()}  "</h1></body></html>`);
 });
