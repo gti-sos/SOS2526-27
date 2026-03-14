02225abe39b71e5a3aa8cf26cb7469630b1f5f77
adrianperez17 <adrpersan@alum.us.es>
2026-03-15 00:01:59 +0100

    Añadido presentacion html

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "about.html"));
