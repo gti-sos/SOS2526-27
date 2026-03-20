7562b620d8dba1798771404b50f5a3554107730b
adrianperez17 <adrpersan@alum.us.es>
2026-03-20 12:56:47 +0100

    Añadir module

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import cors from 'cors';
 import express from 'express';
-import path from 'path';
+import { fileURLToPath } from 'url'; 
+import { dirname } from 'path';  
 import {handler} from './src/front/build/handler.js';
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const port = process.env.PORT || 10000;
-
-
 
 app.use(cors());
 
