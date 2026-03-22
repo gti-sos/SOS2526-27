86a7023212b00c814c5e6dcb4adce791b8b38cc6
adrianperez17 <adrpersan@alum.us.es>
2026-03-22 12:32:10 +0100

    .

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, "public")));
+/*app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "about.html"));
 });
-
+*/
 //BLOQUE ACS
 import drinkingWaterServicesV1 from './src/back/drinking-water-services-v1.js';
 
